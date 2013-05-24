40ce5fd157c0a4bf2e53dfd353f9dce357ad5560
John Demme <jdd@cs.columbia.edu>
2013-05-24 00:03:24 -0400

    Add simulation to test

--- test.py
+++ test.py
@@ -12,16 +12,20 @@ pwl = VPwl(d, [(0, 0),
                ("5n", 1.0),
                ("10n", 0)])
 i1 = Inverter(d)
-r1 = Resistor(d, 10000)
+# r1 = Resistor(d, 10000)
 c1 = Capacitor(d, 2e-15)
 
 d.pair({i1.input: pwl.plus,
         pwl.minus: d.vss,
-        i1.output: r1.a,
-        r1.b: d.vss,
+        # i1.output: r1.a,
+        # r1.b: d.vss,
         c1.pos: i1.output,
-        c1.neg: d.vss})
+        c1.neg: d.vss
+        })
 
 # print n1.gate.__dict__
 
 d.print_netlist(sys.stdout)
+
+s = Simulation("10n", "test.raw")
+s.print_netlist(sys.stdout)
